d25e4b191bfdbc972ca984c33b4bc3cc3f02f8f4
anuragkaushik-glitch <anurag.kaushik@newtonschool.co>
2026-02-24 14:45:55 +0530

    Allow react to take multiple args

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin({})]
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin({})]
   },
   renderer: {
     resolve: {
@@ -15,6 +15,6 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react({})]
   }
 })
